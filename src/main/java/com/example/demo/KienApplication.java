80f47392c1ce1a4769718bbd9f2d0e65cfd6c12b
KIEN <trungkiendo6@gmail.com>
2023-05-27 22:19:14 +0700

    test

---
+++ src/main/java/com/example/demo/KienApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KienApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KienApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -0,0 +1,51 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.Book;
+import com.example.demo.services.BookService;
+import com.example.demo.services.CategoryService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@RequestMapping("/books")
+@Controller
+public class BookController {
+    @Autowired
+    private BookService bookService;
+
+    @Autowired
+    private CategoryService categoryService;
+
+
+    @GetMapping
+    public String listBooks(Model model){
+        List<Book> books = bookService.getAllBooks();
+        model.addAttribute("books",books);
+        return "book/list";
+    }
+
+    @GetMapping("/add")
+    public String addBookForm(Model model){
+        model.addAttribute("book", new Book());
+        model.addAttribute("categories", categoryService.getAllCategories());
+        return "book/add";
+    }
+
+    @PostMapping("/add")
+    public String addBook(@Valid @ModelAttribute("book") Book book, BindingResult bindingResult, Model model){
+        if(book == null || bindingResult.hasErrors()){
+            model.addAttribute("categories", categoryService.getAllCategories());
+            return "book/add";
+        }
+        bookService.addBook(book);
+        return "redirect:/books";
+    }
+}
